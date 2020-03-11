354910ebb9d52a2382c56ef09b5f4effabe68236
Brittany Hasty <Brittany.R.Hasty@gmail.com>
2020-03-10 22:21:38 -0700

    A little lite SQL homework assignment

--- W4D4/DessertRSpec/spec/dessert_spec.rb
+++ W4D4/DessertRSpec/spec/dessert_spec.rb
@@ -1,67 +1,75 @@
 require 'rspec'
 require 'dessert'
+require 'chef'
 
 =begin
 Instructions: implement all of the pending specs (the `it` statements without blocks)! Be sure to look over the solutions when you're done.
 =end
 
 describe Dessert do
-  let(:chef) { Chef.new("") }
-  let(:dessert) {Dessert.new("", 0, chef)}
+  let(:dessert) {Dessert.new("brulee", 10, Chef.new("chef"))}
 
   describe "#initialize" do
-    it "sets a type"
-      chef.type = "sous chef"
-      expect(chef.type).to eq("sous chef")
-    it "sets a quantity"
-      chef.quantity = 10
-      expect(chef.quantity).to eq(10)
-    it "starts ingredients as an empty array"
-      expect(chef.ingredients).to eq([])
-    it "raises an argument error when given a non-integer quantity"
-      expect do
-        chef.quantity = "a metric ton"
-      end.to raise_error(ArgumentError)
+    it "sets a type" do
+      expect(dessert.type).to eq("brulee")
+    end
+
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(10)
+    end
+    
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to eq([])
+    end
+    
+    it "raises an argument error when given a non-integer quantity" do
+      expect {dessert.quantity = "a metric ton"}.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
-      chef.add_ingredient("vanilla")
-      expect(chef.ingredients).to eq(["vanilla"])
+    it "adds an ingredient to the ingredients array" do
+      dessert.add_ingredient("vanilla")
+      expect(dessert.ingredients).to eq(["vanilla"])
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
-      chef.add_ingredient("vanilla")
-      chef.add_ingredient("eggs")
-      chef.add_ingredient("milk")
-      chef.add_ingredient("heavy cream")
-      chef.add_ingredient("sugar")
-      chef.mix!
-      expect(chef.ingredients).not_to eq(["vanilla", "eggs", "milk", "heavy cream", "sugar"])
+    it "shuffles the ingredient array" do
+      dessert.add_ingredient("vanilla")
+      dessert.add_ingredient("eggs")
+      dessert.add_ingredient("milk")
+      dessert.add_ingredient("heavy cream")
+      dessert.add_ingredient("sugar")
+      dessert.mix!
+      expect(dessert.ingredients).not_to eq(["vanilla", "eggs", "milk", "heavy cream", "sugar"])
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
-      chef.quantity = 10
-      chef.eat(2)
-      expect(chef.quantity).to eq(8)
-    it "raises an error if the amount is greater than the quantity"
-      expect do  
-        chef.quantity = 10
-        chef.eat(12)      
-      end.to raise_error("not enough left!")
+    it "subtracts an amount from the quantity" do
+      dessert.quantity = 10
+      dessert.eat(2)
+      expect(dessert.quantity).to eq(8)
     end
 
+    it "raises an error if the amount is greater than the quantity" do
+      dessert.quantity = 10
+      expect{dessert.eat(12)}.to raise_error("not enough left!")
+    end
+  end
+
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
-      chef.name = "boyardee"
-      expect(chef.titleize).to eq("Chef Boyardee the Great Baker")
+    it "contains the titleized version of the chef's name" do
+      dessert.name = "boyardee"
+      expect(chef.titleize).to eq("dessert Boyardee the Great Baker")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
-      chef.bake("Creme Brulee")
-      expect(chef.temp).to eq(400)
+    it "calls bake on the dessert's dessert with the dessert passed in" do
+      chef.bake(dessert)
+      expect(dessert.temp).to eq(400)
+    end
   end
 end
